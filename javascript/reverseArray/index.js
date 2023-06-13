98d85820d36dd45d07f0e6728d1f465dd8927ccb
=armada6199 <moh.abdin27@gmail.com>
2023-06-13 18:35:12 +0300

    adding whiteboard

---
+++ javascript/reverseArray/index.js
@@ -0,0 +1,14 @@
+module.exports= class ReverseArray{
+  constructor(){
+
+  }
+  reverseArray(arr){
+    let newArr=[];
+    for (let i=arr.length-1;i>=0;i--){
+      newArr.push(arr[i]);
+    }
+    return newArr;
+  }
+};
+
+
